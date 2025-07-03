f6b5e97cf86f2cae758dece474f2769999e16e5d
Eugenio Soria <eugeniosoria@gmail.com>
2025-07-03 18:54:03 -0300

    Persistencia

---
+++ Cine/src/cine/persistencia/PersistenciaDatos.java
@@ -0,0 +1,30 @@
+package cine.persistencia;
+
+import cine.modelo.Cine;
+import java.io.*;
+
+public class PersistenciaDatos {
+    private static final String ARCHIVO = "cine.ser";
+
+    public static void guardar(Cine cine) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ARCHIVO))) {
+            oos.writeObject(cine);
+        } catch (IOException e) {
+            System.out.println("ERROR GUARDANDO DATOS: " + e.getMessage());
+        }
+    }
+
+    public static Cine cargar() {
+        File archivo = new File(ARCHIVO);
+        if (!archivo.exists()) {
+            return new Cine(); 
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ARCHIVO))) {
+            return (Cine) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("ERROR CARGANDO DATOS: " + e.getMessage());
+            return new Cine(); 
+        }
+    }
+}
